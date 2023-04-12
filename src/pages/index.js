22fd4f37bc04fe61e42f7df196a08c3485c71bec
prgVaibhav <vaibhav2k1507@gmail.com>
2023-04-12 08:36:37 +0530

    Added custom preview link

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,12 @@ export default function Home() {
         <title>Vaibhav Kumar</title>
         <meta name="description" content="Portfolio website of Vaibhav Kumar" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Vaibhav Kumar" />
+        <meta
+          property="og:description"
+          content="Portfolio website of Vaibhav Kumar"
+        />
+        <meta property="og:image" content="/preview.png" />
         <link rel="icon" href="/favicon.png" />
       </Head>
       <div>
