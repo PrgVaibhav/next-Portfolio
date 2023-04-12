55cd344381b91a11f2bd575abfc1cd94c1a726bb
prgVaibhav <vaibhav2k1507@gmail.com>
2023-04-12 09:01:25 +0530

    Custom Preview Link Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <Head>
         <title>Vaibhav Kumar</title>
+        <meta name="title" content="Vaibhav Kumar" />
         <meta name="description" content="Portfolio website of Vaibhav Kumar" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="Vaibhav Kumar" />
@@ -12,6 +13,13 @@ export default function Home() {
           property="og:description"
           content="Portfolio website of Vaibhav Kumar"
         />
+        <meta property="og:url" content="https://vaibhavkumar.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Vaibhav Kumar" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Vaibhav Kumar" />
+        <meta name="twitter:creator" content="@SyntaxError408" />
         <meta property="og:image" content="/preview.png" />
         <link rel="icon" href="/favicon.png" />
       </Head>
