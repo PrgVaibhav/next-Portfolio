56b892934ce7f3ece1b8f52b3bb464a065097537
prgVaibhav <vaibhav2k1507@gmail.com>
2023-04-13 13:05:39 +0530

    Added vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
