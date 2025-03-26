ed877f4c9b69e69a7fd53df658dfd391a3eae4af
Vaibhav <vaibhav@vbv.webdev>
2025-03-26 11:05:51 +0530

    vercel error fixing

--- src/pages/updates/Updates.tsx
+++ src/pages/updates/Updates.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Updates = () => {
   return (
     <section className="card-color p-4 sm:p-[1.4vw] border border-neutral-700 rounded-lg sm:rounded-[.7vw] flex flex-col gap-8 sm:gap-[3vh]">
